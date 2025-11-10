5bc530a2fc7026e999d7cfd1bd09ae1f60111953
zeca-sas <josecmirandabonelli@gmail.com>
2025-11-10 19:41:23 -0300

    Update 5Quick_Sort.py

--- 5Quick_Sort.py
+++ 5Quick_Sort.py
@@ -7,8 +7,7 @@ def quick_sort(lista):
     if len(lista) <= 1:
         return lista
     else:
-        pivo = lista[0]
-    
+        pivo = lista[0]  
         menores = [x for x in lista[1:] if x <= pivo]
         maiores = [x for x in lista[1:] if x > pivo]
         return quick_sort(menores) + [pivo] + quick_sort(maiores)
@@ -17,3 +16,4 @@ def quick_sort(lista):
 ordenada = quick_sort(numeros)
 
 print(ordenada)
+
